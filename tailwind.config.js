6cd2aa975ca6af18c2e0b8cf593d27c0b68ad2aa
Kyle Butts <buttskyle96@gmail.com>
2024-01-16 16:47:01 -0500

    Basic template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+// Don't style code blocks 
+// Source: https://www.skcript.com/blog/disable-tailwind-prose-code
+const disabledCss = {
+	'code::before': false,
+	'code::after': false,
+	'blockquote p:first-of-type::before': false,
+	'blockquote p:last-of-type::after': false,
+	pre: false,
+	code: false,
+	'pre code': false,
+	'code::before': false,
+	'code::after': false,
+}
+
 module.exports = {
   content: ["./*.{js,html,qmd,md,Rmd}"],
   theme: {
-    extend: {}
+    extend: {
+      typography: {
+				DEFAULT: { css: disabledCss },
+				sm: { css: disabledCss },
+				lg: { css: disabledCss },
+				xl: { css: disabledCss },
+				'2xl': { css: disabledCss },
+			},
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
